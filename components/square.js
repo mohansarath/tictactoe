a2b45581e9f7fab655c9beed350a0e100b4c4564
sarath <sarath.sct@gmail.com>
2018-03-12 11:25:16 +0530

    board

---
+++ components/square.js
@@ -0,0 +1,11 @@
+import react, { Component } from 'react';
+
+export default class extends Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        )
+    }
+}
---
+++ pages/home.js
@@ -0,0 +1,19 @@
+import react, { Component } from 'react';
+import Board from '../components/board';
+
+
+export default class extends Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        )
+    }
+}
